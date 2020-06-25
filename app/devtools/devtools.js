ec4b7b90769427a66c20e95177bd4d30736b6e01
Koushik <koushik350@gmail.com>
2020-06-25 21:59:07 +0530

    tutorials-devtools

--- app/devtools/devtools.js
+++ app/devtools/devtools.js
@@ -1,39 +1,15 @@
-/**
- * @see https://developer.chrome.com/extensions/examples/api/devtools/panels/chrome-query/devtools.js
- * @see https://developer.chrome.com/extensions/devtools#selected-element
- */
+console.log("Hey I am from devtools");
 
-/**
- * @author Koushik Chatterjee <koushik@letcode.in>
- */
+chrome.devtools.panels.elements.createSidebarPane("LetXPath", () => {
+    console.log("Hey!");
 
-// Create a connection to the background page
-var devtools_connections = chrome.runtime.connect({
-    name: "ortoni_devtools_message"
 });
 
-let name = "LetXPath";
-let icon = "assets/32.png"
-let html = "devtools.html";
-let show = () => {
-    // chrome.extension.sendMessage({ type: "getXPath" });
-}
-chrome.devtools.panels.elements.createSidebarPane(name, (panel) => {
-    chrome.devtools.panels.elements.onSelectionChanged.addListener(() => {
-        chrome
-            .devtools
-            .inspectedWindow
-            .eval("parseDOM($0)", {
-                useContentScriptContext: true
-            });
-        chrome.tabs.sendMessage(chrome.devtools.inspectedWindow.tabId, { request: "element_change" })
-    });
-
-    panel.setPage('panel.html');
-    // panel.
-});
-devtools_connections.postMessage({
-    name: 'ortoni_devtools_message_start',
-    tabId: chrome.devtools.inspectedWindow.tabId
-});
-
+// demo - we not gonna use this
+// chrome.devtools.panels.create("LetXPath",
+//     "MyPanelIcon.png",
+//     "Panel.html",
+//     function (panel) {
+//         // code invoked on panel creation
+//     }
+// );
