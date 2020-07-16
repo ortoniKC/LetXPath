bd2d59ab5e0a04eb68db1859e430a6cfc6a1f358
Koushik <koushik350@gmail.com>
2020-07-16 23:46:33 +0530

    https://youtu.be/7KQjVeRqL-0

--- eventPage.js
+++ eventPage.js
@@ -1,9 +1,34 @@
+
+let installURL = "https://youtu.be/1CISIspFD_U";
+let updateURL = "https://github.com/ortoniKC/LetXPath";
+let uninstallURL = "https://www.letcode.in/products";
+
+chrome.runtime.setUninstallURL(uninstallURL, () => { });
+
+let installReason = (detail) => {
+    console.log(detail);
+
+    if (detail.reason === "install") {
+        chrome.tabs.create({
+            url: installURL
+        })
+    } else if (detail.reason === "update") {
+        chrome.tabs.create({
+            url: updateURL
+        })
+    }
+
+}
+chrome.runtime.onInstalled.addListener((details) => {
+    installReason(details)
+})
+
+
 chrome.contextMenus.create({
     "id": "LetXPath",
     "title": "Get XPath",
     "contexts": ["all"]
 })
-
 let getXPath = (info, tab) => {
     let msg = {
         type: 'getXPath'
@@ -12,9 +37,8 @@ let getXPath = (info, tab) => {
         console.log("Message sent");
     })
     console.log(tab);
-
-
 }
+
 chrome.contextMenus.onClicked.addListener((info, tab) => {
     getXPath(info, tab)
 })
